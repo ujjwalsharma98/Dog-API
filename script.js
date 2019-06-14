58e2a06eac6ee1049595e8e1b4a9e2207b79ae00
ujjwalsharma98 <us36335@gmail.com>
2019-06-14 11:32:04 +0530

    This was my first API hit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function fetchRandomDogImage(){
+    var xhrRequest = new XMLHttpRequest();
+
+    xhrRequest.onload = function(){
+        console.log(xhrRequest.response);
+        var responseJSON = JSON.parse(xhrRequest.response);
+        var imageUrl = responseJSON.message;
+        $('#dog-image').attr('src' , imageUrl);
+    }
+    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random');
+    xhrRequest.send();
+}
+
+
+$('#fetch-button').click(fetchRandomDogImage);
